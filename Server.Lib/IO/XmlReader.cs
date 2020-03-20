d92e6d7e1559bb7b76047105829c8a53340d5fb4
Vitaclick <vitaliy.zema@gmail.com>
2020-03-20 15:37:42 +0300

    lib fix

---
+++ Server.Lib/IO/XmlReader.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Server.Lib.IO
+{
+  public class XmlReader
+  {
+    static void FromXmlFile(string filePath)
+    {
+
+    }
+  }
+}
